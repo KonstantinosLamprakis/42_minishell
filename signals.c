78fa8ee449835193137566203c5f3207e57a4a87
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-17 10:08:22 +0200

    Adding some comments

--- signals.c
+++ signals.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/16 09:56:54 by klamprak          #+#    #+#             */
-/*   Updated: 2024/04/16 10:47:44 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/17 10:07:57 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,7 @@
 info:
 	- we can not pass any data to signal handler(its forbitten at task description) so we need to clean everything each time a command is finished
 	so we will be ready to terminate the program as soon as we get a ctrl+D signal
+	- its ok if you don't free some things like envp[][] before you exit as soon as you have not access to main variables
 
 Shell:
 	- https://stackoverflow.com/questions/18530572/how-to-execute-a-command-from-c-and-terminate-it: send signal to proccess after fork
