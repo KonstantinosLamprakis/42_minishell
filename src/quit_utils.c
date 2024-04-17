4777eb1077e4a8f394acc434c9a0f6ed15f55ffd
Louen Greau <lgreau@3-A-3.42heilbronn.de>
2024-04-17 11:13:19 +0200

    General free function

---
+++ src/quit_utils.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   quit_utils.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 11:00:08 by lgreau            #+#    #+#             */
+/*   Updated: 2024/04/17 11:05:08 by lgreau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+/**
+ * @brief Frees everything in arr until NULL is reached
+ *
+ * @param arr
+ * @param is_alloc whether or not arr is allocated
+ */
+void	free_arr(void **arr, int is_alloc)
+{
+	if (arr)
+		while (*arr)
+			free(*arr++);
+	if (is_alloc)
+		free(arr);
+}
