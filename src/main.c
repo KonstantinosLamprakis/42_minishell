ab1198290c53574aed9d7da61cd07b36f36f0dc5
Louen Greau <lgreau@3-A-3.42heilbronn.de>
2024-04-17 12:03:02 +0200

    main entrance point

---
+++ src/main.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/17 10:05:38 by lgreau            #+#    #+#             */
+/*   Updated: 2024/04/17 10:51:36 by lgreau           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+int	main(int argc, char *argv[], char *envp[])
+{
+	t_program	program;
+
+	init_struct(&program, envp);
+
+	exit(*get_errno() != 0);
+}
