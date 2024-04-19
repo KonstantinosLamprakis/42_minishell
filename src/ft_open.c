f3a993d35f023f417bd331f8f608afd99444d8eb
Louen Greau <lgreau@2-C-8.42heilbronn.de>
2024-04-19 13:36:41 +0200

    ft_open integrating statis t_program changes

--- src/ft_open.c
+++ src/ft_open.c
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:23:33 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/17 14:31:47 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/04/19 09:51:34 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,17 @@
  * @brief Overriding open to save the file descriptor in program
  * for easy cleanup
  *
- * @param program
  * @param file_name to open
  * @param flags to open the file with
  * @param mode optionnal (= -1) mode to open the file with
  * @return int
  */
-int	ft_open(t_program *program, char *file_name, int flags, int mode)
+int	ft_open(char *file_name, int flags, int mode)
 {
-	int	fd;
+	t_program	*program;
+	int			fd;
 
+	program = get_program();
 	if (mode < 0)
 		fd = open(file_name, flags);
 	else
