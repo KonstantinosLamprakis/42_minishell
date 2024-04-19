31a6941cc0f31d7d6b50092e8aab8dcdc03206ff
Louen Greau <lgreau@2-C-8.42heilbronn.de>
2024-04-19 13:30:01 +0200

    Fixed getnth word

--- includes/libft/src/ft_string/ft_getnth_word.c
+++ includes/libft/src/ft_string/ft_getnth_word.c
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/19 11:44:37 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/19 12:07:13 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/04/19 13:27:01 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,21 +32,20 @@ char	*ft_getnth_word(char *str, int n, int (*is_sep)(int), int (*cmp)(int))
 	if (!str || n < 0 || !is_sep)
 		return (set_error((char *)__func__, INVALID_ARG), NULL);
 	index = -1;
+	n -= !(*is_sep)(str[0]);
+	start = !(*is_sep)(str[0]);
 	while (str[++index] && n >= 0)
 	{
 		if (ft_isword(str + index, is_sep))
-		{
-			if (n == 1)
-				start = index + 1;
-			n--;
-		}
+			if (n-- == 1)
+				start = index + (*is_sep)(str[index]);
 	}
 	if (n > 1)
 		return (NULL);
-	if (cmp)
-		word = ft_substr(str, start, index - start - 1);
+	if (!cmp)
+		word = ft_substr(str, start, index - start);
 	else
-		word = ft_substr_if(str, start, index - start - 1, cmp);
+		word = ft_substr_if(str, start, index - start, cmp);
 	if (*get_errno() != 0)
 		return (NULL);
 	return (word);
