8b624063b1eb2be5d78681a02fb2567d6dae373d
Louen Greau <lgreau@2-C-8.42heilbronn.de>
2024-04-19 13:38:30 +0200

    Added the NO_OPERATOR

--- includes/libft/src/ft_parser/ft_parser.h
+++ includes/libft/src/ft_parser/ft_parser.h
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/14 11:18:11 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/19 11:37:01 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/04/19 13:37:27 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,6 +34,7 @@ typedef enum e_operators
 
 typedef enum e_encapsulators
 {
+	NO_OPERATOR,
 	L_PARANTHESE,
 	R_PARANTHESE,
 	L_CBRACKET,
