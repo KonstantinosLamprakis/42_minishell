ffaa75ba730891575335c038e954e362ff090c3b
Louen Greau <lgreau@2-C-8.42heilbronn.de>
2024-04-19 13:30:25 +0200

    Updated header & Makefile

--- includes/minishell.h
+++ includes/minishell.h
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 16:38:40 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/18 11:41:47 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/04/19 11:38:08 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,23 +34,24 @@ typedef struct s_program
 
 //			STRUCT_RELATED
 
-void		init_struct(t_program *program, char **envp);
+t_program	*get_program(void);
+void		init_struct(char **envp);
 void		clean_struct(t_program *program);
 
 
 //			UTILS
 
 void		free_arr(void **arr, int is_alloc);
-int			ft_open(t_program *program, char *file_name, int flags, int mode);
+int			ft_open(char *file_name, int flags, int mode);
 
 //			OPERATORS
 
-void		l_redirect_handler(void *arg);
-void		left_redirection(t_program *program, char *arg, char *left_arg);
+int		l_redirect_handler(void *arg);
+void		left_redirection(char *arg, char *left_arg);
 
 //			MISC.c
 
-void		print_opened_fd(t_program *program);
-void		print_std_fd(t_program *program);
+void		print_opened_fd(void);
+void		print_std_fd(void);
 
 #endif
