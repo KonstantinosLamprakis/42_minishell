c9cad1bb64cb4d875c60fd3f6b38155f6a81c4f8
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-24 19:02:52 +0200

    Taking notes about new bugs that I should fix

--- src/main.c
+++ src/main.c
@@ -6,11 +6,26 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 10:05:38 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/24 07:36:27 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/24 19:02:32 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+/*
+	tester: https://github.com/zstenger93/42_minishell_tester
+	bugs:
+		- export name+=value(to existing and new ones vars)
+		- SHLVL: equal to +1 if valid or 1 if invalid(ex negative or string)
+		- CDPATH
+		- commands with uppercase
+		- error msgs to be the same
+		- check for mem leaks
+		- fix norminette
+		- ~ should give home dir even if HOME unseted
+		- OLDPWD doesn't exists initally
+		- error when pwd takes args
+ */
 
 int	main(int argc, char *argv[], char *envp[])
 {
