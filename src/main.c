49a2cd7c9b4f8acb9822114ce1e070442417c4bb
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-27 19:28:09 +0200

    Updating the description for the bugs that are still pending

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 10:05:38 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/27 19:23:47 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/27 19:27:40 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,18 +15,10 @@
 /*
 	tester: https://github.com/zstenger93/42_minishell_tester
 	bugs:
-		- export test="" -> adds multiple "", same export test="test1"
-			- maybe split's return value should also include "", and not remove
-			them
-		- echo " test -> seg fault
-		- export test_3421 -> not valid identifirer
 		- export test1="" => not valid identifier -> split gives a NULL arg extra
 			- I fixed that in my code(b_export()), but I thing is parser error
-		- parser error when provides a wrong command, then you can not continue
-		- "echo test"
-		- seg fault when command not found
+		- "echo test" -> wrong error number instead of command not found
 		- ctrl D should decrease SHLVL if its > 1 and clean everything
-		- check the output of put_signal_handler for errors
 	todo:
 		- clean_struct should clean everything because we use it at signals and at exit
 		- delete and clean git branches
