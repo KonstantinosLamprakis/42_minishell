737fd0b5f22b2b202656d26ba8dd7cf70143d26b
KonstantinosLamprakis <28678729+KonstantinosLamprakis@users.noreply.github.com>
2024-04-29 09:05:58 +0200

    Fixing exit status to be 127 when command not found

--- src/cmd_handler.c
+++ src/cmd_handler.c
@@ -6,7 +6,7 @@
 /*   By: klamprak <klamprak@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/23 09:39:02 by lgreau            #+#    #+#             */
-/*   Updated: 2024/04/28 23:02:13 by klamprak         ###   ########.fr       */
+/*   Updated: 2024/04/29 09:01:46 by klamprak         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,6 +35,7 @@ char	*get_cmd(char **cmd_args)
 			free(cmd);
 		}
 	}
+	program->status = 127;
 	return (set_error((char *)__func__, COMMAND_NOT_FOUND), NULL);
 }
 
