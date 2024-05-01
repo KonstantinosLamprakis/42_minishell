a34762422461d6b1a3f1305b91ec1d6694703635
Louen Greau <lgreau@3-A-3.42heilbronn.de>
2024-05-01 15:12:08 +0200

    Fixed > and >> parsing order and skip

--- src/left_redirection.c
+++ src/left_redirection.c
@@ -6,7 +6,7 @@
 /*   By: lgreau <lgreau@student.42heilbronn.de>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:06:09 by lgreau            #+#    #+#             */
-/*   Updated: 2024/05/01 10:47:41 by lgreau           ###   ########.fr       */
+/*   Updated: 2024/05/01 15:04:06 by lgreau           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -106,7 +106,7 @@ int	l_redirect_handler(void *arg)
 		free(left_arg);
 	if (!sub_right)
 		return (close(fd), -1);
-	return (ft_parse(sub_right), free(sub_right), -1);
+	return (ft_parse(sub_right), free(sub_right), close(fd), -1);
 }
 
 /**
@@ -120,7 +120,7 @@ int	left_redirection(char *arg)
 {
 	int	right_fd;
 
-	right_fd = ft_open(arg, O_RDONLY, -1);
+	right_fd = ft_open(arg, O_RDONLY | O_TRUNC, -1);
 	if (right_fd < 0)
 		return (-1);
 	if (dup2(right_fd, STDIN) < 0)
